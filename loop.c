abe7027c624121c81944c8a58915c5f0f0bcff9b
Sanketh-Patil09 <sanketh.patil09@gmail.com>
2024-02-23 21:07:34 +0530

    new changes

--- loop.c
+++ loop.c
@@ -1,3 +1,9 @@
+/*For each integer n in the interval [a,b] (given as input) :
+
+If 1<=n<=9, then print the English representation of it in lowercase. That is "one" for 1, "two" for 2, and so on.
+Else if  n>9and it is an even number, then print "even".
+Else if  n>9and it is an odd number, then print "odd".
+Note: [a,b]={x belongs to Z| a<=x<=b}={a,a+1,....,b}*/
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
