8e80f15900cd4cfcb67804caedeccb6b8377c737
Sanketh-Patil09 <sanketh.patil09@gmail.com>
2024-02-23 20:59:00 +0530

    new changes

--- pointer.c
+++ pointer.c
@@ -1,3 +1,7 @@
+/*Complete the function void update(int *a,int *b). It receives two integer pointers, int* a and int* b. Set the value of a' to their sum, and b' to their absolute difference. There is no return value, and no return statement is needed.
+i.e.
+a'=a+b
+b'=|a-b|*/
 #include <stdio.h>
 
 void update(int *a,int *b) {
