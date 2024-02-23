8057e5080ad4f989f1995ba293b95d9113363fa3
Sanketh-Patil09 <sanketh.patil09@gmail.com>
2024-02-23 21:02:38 +0530

    new changes

--- conditional_statement.c
+++ conditional_statement.c
@@ -1,56 +1,55 @@
+/*Given a positive integer denoting n, do the following:
+
+If 1<=n<=9, print the lowercase English word corresponding to the number (e.g., one for 1, two for 2, etc.).
+If n>9, print Greater than 9.
+Input Format*/
 #include<stdio.h>
 #include<stdlib.h>
-void statement_print(int n);
+
 int main()
 {
     int n;
-    if(n>=1&&n<=10^9)
-    {
         scanf("%d",&n);
-        statement_print(n);
-    }
-    else
-    {
-        exit(05);
-    }
-}
-void statement_print(int n)
-{
-    if(n>=1&&n<=9)
-    {
-        switch(n)
+        if(n==1)
         {
-            case 1:
-            printf("one\n");
-            break;
-            case 2:
-            printf("two\n");
-            break;
-            case 3:
-            printf("three\n");
-            break;
-            case 4:
-            printf("four\n");
-            break;
-            case 5:
-            printf("five\n");
-            break;
-            case 6:
-            printf("six\n");
-            break;
-            case 7:
-            printf("seven\n");
-            break;
-            case 8:
-            printf("eight\n");
-            break;
-            case 9:
-            printf("nine\n");
-            break;
+            printf("one");
         }
-    }
-    else if(n>9)
-    {
-        printf("Greater than 9");
-    }
+        else if(n==2)
+        {
+            printf("two");
+        }
+        else if(n==3)
+        {
+            printf("three");
+        }
+        else if(n==4)
+        {
+            printf("four");
+        }
+        else if(n==5)
+        {
+            printf("five");
+        }
+        else if(n==6)
+        {
+            printf("six");
+        }
+        else if(n==7)
+        {
+            printf("seven");
+        }
+        else if(n==8)
+        {
+            printf("eight");
+        }
+        else if(n==9)
+        {
+            printf("nine");
+        }
+        else if(n>9)
+        {
+            printf("Greater than 9");
+        }
+    
+    
 }
