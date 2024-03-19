f1dd751a959eb08be7461adf3338aa55bda10303
Sanketh Patil <160312145+Sanketh-Patil09@users.noreply.github.com>
2024-03-19 13:48:36 +0530

    new

--- Programs/welcome.c
+++ Programs/welcome.c
@@ -1,15 +1,6 @@
-//You have to print the character, ch, in the first line. Then print s in next line. In the last line print the sentence, sen.
-#include <stdio.h>
-#include <string.h>
-
-int main() {
-    char ch;
-    char s[100], sen[100]; 
-    
-    scanf(" %c", &ch); 
-    scanf("%s", s); 
-    scanf(" %[^\n]", sen); 
-    
-    printf("%c\n%s\n%s\n", ch, s, sen);  
+#include<stdio.h>
+int main()
+{
+    printf("Welcome to C!!");
     return 0;
 }
